ac48930888a653cd71ffa7622f4a6b0548f490df
Trash Can <kinetic.0212@gmail.com>
2026-07-12 09:39:26 +0530

    reading and writing to json config file

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"github.com/K1N3tiCs/gator/internal/config"
+	"log"
+)
 
 func main() {
-	log.Fatal("Hello, World!")
+	jsonConfig, err := config.Read()
+	if err != nil {
+		log.Fatal("failed reading the json config file")
+	}
+	fmt.Println(jsonConfig)
+
+	if err := jsonConfig.SetUser("Penguuu"); err != nil {
+		log.Fatal("failed to set user")
+	}
+
+	fmt.Println(jsonConfig)
 }
