bc1ad5abdce4c82e389d6cada87f951034ad8f55
Trash Can <kinetic.0212@gmail.com>
2026-07-15 10:51:16 +0530

    no changes

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
+
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerDeleteAll)
